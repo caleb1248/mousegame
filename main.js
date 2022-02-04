99cbf21a9ffa4294efbcab6dede1eb9dfaaf3f22
Caleb <classicalcaleb@gmail.com>
2022-02-04 13:43:57 -0500

    stuff

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const player = new Player(ctx);
 player.render();
+function frame(){
+  reqestAnimationFrame(frame);
+  player.render()
+}
