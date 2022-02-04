94639a58b2949996b6ffb61e9d905d6e7869160e
Caleb <classicalcaleb@gmail.com>
2022-02-04 16:43:50 -0500

    just saving

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 import './style.css'
 import Player from './player'
 const canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 const player = new Player(ctx);
-player.render();
 function frame(){
-  reqestAnimationFrame(frame);
+  requestAnimationFrame(frame);
+  ctx.clearRect(0,0,window.innerWidth,window.innerHeight);
+  player.update();
   player.render();
 }
-frame()
+frame();
+canvas.addEventListener('mousemove', player.handleMouseMove.bind(player))
