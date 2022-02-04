46c1a916e47844ebb2ba69555a05791554a8e879
Caleb <classicalcaleb@gmail.com>
2022-02-04 13:38:16 -0500

    fixed the playerjs bug

--- player.js
+++ player.js
@@ -1,8 +1,9 @@
 export default class Player{
     constructor(canvas){
         this.position = {x:0,y:0};
+        this.canvas = canvas
     }
     render(){
-        canvas.fillRect(this.position.x,this.position.y);
+        this.canvas.fillRect(this.position.x,this.position.y, 10, 10);
     }
 }
